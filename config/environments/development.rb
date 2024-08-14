774eea53f22d89b8dd47fa144c215b759a1e28f4
Mehmet <mehmetkusken@gmail.com>
2024-08-14 16:20:40 -0600

    added domain link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: 'stark-reaches-11084-430d0fee8cd5.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'stark-reaches-11084-430d0fee8cd5.herokuapp.com'"http://www.goeswebdesigns.com"}
 end
