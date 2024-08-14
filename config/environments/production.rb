df220c1781904504a7c3ff8098fc467212159593
Mehmet <mehmetkusken@gmail.com>
2024-08-14 15:47:05 -0600

    added domain link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_mailer.default_url_options = { host: 'stark-reaches-11084-430d0fee8cd5.herokuapp.com','http://www.goeswebdesigns.com' }
+  config.action_mailer.default_url_options = { host: 'stark-reaches-11084-430d0fee8cd5.herokuapp.com','www.goeswebdesigns.com' }
 end
